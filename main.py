3140c1f82214f16e9e666757ee82776ecc43d77b
Zyto_Rex <jacob.wolford@gmail.com>
2021-07-27 14:54:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+#Made by JWolf
+#import turtle
+from turtle import Turtle,Screen
+from random import randint
+
+
+#define main function 
+def main():
+  #declare imports as variables
+  t=Turtle()
+  s = Screen()
+#set bg color
+  s.bgcolor('Black')
+
+  colors: list = ['hotpink','orange','cyan']
+
+  for dot in range():
+    
+
+  s.mainloop()
+
+
+
+
+
+main()
