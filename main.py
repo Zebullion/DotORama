e34865d45f985075aaa15454f3ceb4cae7db57c8
Zyto_Rex <jacob.wolford@gmail.com>
2021-07-27 15:24:02 +0000

    Added logic and color/size dot randomization

--- main.py
+++ main.py
@@ -9,13 +9,28 @@ def main():
   #declare imports as variables
   t=Turtle()
   s = Screen()
-#set bg color
+#set bg color and tweak turtle
   s.bgcolor('Black')
+  t.speed(0)
+  t.penup()
 
   colors: list = ['hotpink','orange','cyan']
 
-  for dot in range():
-    
+  for dot in range(1000):
+    x =  randint(-100,100)
+    y = randint(-100,100)
+    t.goto(x,y)
+    size = randint(1,10)
+
+    if x<-50:
+      t.dot(size,colors[0])
+    elif x>=-50 and x<0:
+      t.dot(size,colors[1])
+    elif x>=0 and x<=50:
+      t.dot(size,colors[2])
+    elif x>=50:
+      t.dot(size,colors[0])
+
 
   s.mainloop()
 
